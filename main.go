cbcfe134a604ba02237c25cf9f23e451eb2ee4e9
Kyle OBrien <obrienk@webbhost.net>
2021-01-29 03:42:32 -0600

    setup cobra initialization

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "github.com/spf13/cobra"
+
+func main() {
+  fmt.Println("")
+  fmt.Println("  Orphic, INCORPORATED. Query - CLI - API - e !")
+  fmt.Println("  The only normal people are the ones you don't know very well  ")
+  fmt.Println("")
+  fmt.Println("Orphic Golang CLI Coming Soon...https://github.com/orphic-inc/go-orphic-cli")
+  fmt.Println("")
+  fmt.Println("You've stumbled upon a door where your mind is the key.")
+  fmt.Println("")
+  fmt.Println("  The Golang Orphic CLI was created during a Hackathon from Friday January 29 - Feb 1 2021 by Kyle O\`'`Brien \\ Orphic INCORPORATED`Some Rights Reserved. MIT License 2021 Orphic INCORPORATED`")
+  fmt.Println("")
+}
+import (
+  "GoOrphicCli/cmd"
+)
+
+func main() {
+  cmd.Execute()
+}
